ffcbc5db4494ca85f4691e13831d3f9e1cdbad29
Kristen Holtz <holtz.kristen@gmail.com>
2018-03-15 00:47:14 -0700

    More edges and masks...

---
+++ lines_code.py
@@ -0,0 +1,96 @@
+import math
+
+def get_slopes(lines):
+    """
+    Go through array of lines and return array of corresponding slopes
+    """
+    m = []
+    for line in lines:
+        denom = line[0,2] - line[0,0]
+        #if denom == 0:
+        #    denom = 1e-9
+        m.append((line[0,3]-line[0,1]) / denom)
+        if (m[-1]*0) != 0:
+            print("slope is NaN: ", m, line)
+    return np.array(m)
+
+def remove_outliers(slopes):
+    """
+    Go through array of slopes and remove outliers, inf, and nan. Return array of valid indices.
+    """
+    
+    top = 2.5*np.percentile(slopes, 75) - 1.5*np.percentile(slopes, 25)
+    bottom = 2.5*np.percentile(slopes, 25) - 1.5*np.percentile(slopes, 75)
+    
+    valid_inds = []
+    for ind in range(len(slopes)):
+        s = slopes[ind]
+        if abs(s) > 1e9:
+            continue
+        if math.isnan(s):
+            continue
+        if s <= top and s >= bottom:
+            valid_inds.append(ind)
+
+    return valid_inds
+    
+
+def get_intercept(mean_slope, lines):
+    """
+    Go through array of lines and return mean intercept given the slope of the line as mean_slope
+    """
+    b = []
+    for line in lines:
+        b.append(line[0,1] - mean_slope*line[0,0])
+        b.append(line[0,3] - mean_slope*line[0,2])
+    return np.mean(b)
+        
+
+def get_lane_lines(lines, imsize):
+    """
+    Go through array of lines and find two lane lines
+    """
+    
+    # Get slope of each line and split into two categories (positive and negative slopes)
+    m = get_slopes(lines)
+    posinds = m > 0.1
+    neginds = m < -0.1
+    res = []
+    height = imsize[0]
+    width = imsize[1]
+    
+    # for each of the two groups, remove outliers based on slope then find the mean slope 
+    # of the lines and the mean intercept corresponding to that slope
+    # ignore if there are no lines in that category
+    
+    if sum(posinds) > 0:
+        poslines = lines[posinds]
+        posm = get_slopes(poslines)
+        val_pos_ind = remove_outliers(posm)
+        posm = posm[val_pos_ind]
+        poslines = poslines[val_pos_ind]
+        meanposm = np.mean(posm)
+        posb = get_intercept(meanposm, poslines)
+        if meanposm == 0:
+            meanposm = 1e-10
+        if not math.isnan(posb) and not math.isnan(meanposm):
+            line1 = [[min(max(int((height-posb)/meanposm), 0), width), height, 
+                      min(max(int(((0.6*height)-posb)/meanposm), 0), width), int(0.6*height)]]
+            res.append(line1)
+
+    if sum(neginds) > 0:
+        neglines = lines[neginds]
+        negm = get_slopes(neglines)
+        val_neg_ind = remove_outliers(negm)
+        negm = negm[val_neg_ind]
+        neglines = neglines[val_neg_ind]
+        meannegm = np.mean(negm)
+        negb = get_intercept(meannegm, neglines)
+        if meannegm == 0:
+            meannegm = 1e-10
+        if not math.isnan(negb) and not math.isnan(meannegm):
+            line2 = [[min(max(int((height-negb)/meannegm), 0), width), height, 
+                      min(max(int(((0.6*height)-negb)/meannegm), 0), width), int(0.6*height)]]
+            res.append(line2)
+   
+    return res
--- secret_sauce.py
+++ secret_sauce.py
@@ -10,12 +10,14 @@ class Params:
 		self.red = (0, 255)
 		self.blue = (0, 255)
 		self.green = (0, 255)
-
+		self.low_threshold = 0
+		self.high_threshold = 0
 
 params = Params()
 
 def get_color_widget(color_name):
-	return widgets.IntRangeSlider(value=[0, 255], 
+	return widgets.IntRangeSlider(
+		value=[0, 255], 
 		min=0,
 		max=255,
 		step=1, 
@@ -24,8 +26,7 @@ def get_color_widget(color_name):
 		continuous_update=False,
 		orientation='horizontal',
 		readout=True,
-		readout_format='d'
-		)
+		readout_format='d')
 
 red_widget = get_color_widget('Red')
 blue_widget = get_color_widget('Blue')
@@ -60,18 +61,27 @@ high_widget = widgets.IntSlider(
 filename = 'solidWhiteRight.jpg'
 img = mpimg.imread(filename)
 
-def apply_color_thresholds(red=(0, 255), blue=(0, 255), green=(0, 255)):
-	color_select = np.copy(img)
+yellowfile = 'solidYellowCurve.jpg'
+yellowimg = mpimg.imread(yellowfile)
+
+def apply_color_thresholds(image, red, blue, green):
+	out_of_range = (image[:, :, 0] < red[0]) | (image[:, :, 0] > red[1]) \
+	| (image[:, :, 1] < blue[0]) | (image[:, :, 1] > blue[1]) \
+	| (image[:, :, 2] < green[0]) | (image[:, :, 2] > green[1])
+	result = np.copy(image)
+	result[out_of_range] = [0, 0, 0]
+	return result
+
+def show_color_threshold_results(red=(0, 255), blue=(0, 255), green=(0, 255)):
 	params.red = red
 	params.blue = blue
 	params.green = green
-	thresholds = (img[:,:,0] < red[0]) | (img[:,:,0] > red[1]) \
-	| (img[:,:,1] < blue[0]) | (img[:,:,1] > blue[1]) \
-	| (img[:,:,2] < green[0]) | (img[:,:,2] > green[1])
-	color_select[thresholds] = [0, 0, 0]
-	f, axarr = plt.subplots(1,2, figsize=(20, 10))
-	axarr[0].imshow(img)
-	axarr[1].imshow(color_select)
+
+	f, axarr = plt.subplots(2,2, figsize=(20, 12))
+	axarr[0, 0].imshow(img)
+	axarr[0, 1].imshow(apply_color_thresholds(img, red, blue, green))
+	axarr[1, 0].imshow(yellowimg)
+	axarr[1, 1].imshow(apply_color_thresholds(yellowimg, red, blue, green))
 	plt.show()
 
 def show_rgb():
@@ -98,14 +108,70 @@ def show_rgb():
 	axarr[2].imshow(green_img[:,:,2], cmap='gray')
 	plt.show()
 
-def apply_edges(low_threshold=0, high_threshold=100):
-	gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-	canny = cv2.Canny(gray, low_threshold, high_threshold)
-	f, axarr = plt.subplots(1, 2, figsize=(20,10))
-	axarr[0].imshow(gray, cmap='gray')
-	axarr[1].imshow(canny, cmap='gray')
+def apply_edges(image, low_threshold, high_threshold):
+	gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+	return cv2.Canny(gray, low_threshold, high_threshold)
+
+def show_edge_detection_results(low_threshold=0, high_threshold=0):
+	params.low_threshold = low_threshold
+	params.high_threshold = high_threshold
+	f, axarr = plt.subplots(2, 2, figsize=(20,12))
+	axarr[0, 0].imshow(img, cmap='gray')
+	axarr[0, 1].imshow(apply_edges(img, low_threshold, high_threshold), cmap='gray')
+	axarr[1, 0].imshow(yellowimg, cmap='gray')
+	axarr[1, 1].imshow(apply_edges(yellowimg, low_threshold, high_threshold), cmap='gray')
 	plt.show()
 
-def print_params():
-	print(params.red, params.blue, params.green)
+def apply_masks():
+	lower_color = np.array([params.red[0], params.blue[0], params.green[0]])
+	higher_color = np.array([params.red[1], params.blue[1], params.green[1]])
+	colormask_white = cv2.inRange(img, lower_color, higher_color)
+	colormask_white = cv2.dilate(colormask_white, (10, 10))
+	white_edges = cv2.Canny(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), 
+		params.low_threshold, params.high_threshold)
+	white_masked_edges = cv2.bitwise_and(white_edges, colormask_white)
 
+	colormask_yellow = cv2.inRange(yellowimg, lower_color, higher_color)
+	colormask_yellow = cv2.dilate(colormask_yellow, (10, 10))
+	yellow_edges = cv2.Canny(cv2.cvtColor(yellowimg, cv2.COLOR_RGB2GRAY), 
+		params.low_threshold, params.high_threshold)
+	yellow_masked_edges = cv2.bitwise_and(yellow_edges, colormask_yellow)
+
+	f, axarr = plt.subplots(2,2, figsize=(20, 12))
+	axarr[0, 0].imshow(img)
+	axarr[0, 1].imshow(white_masked_edges, cmap='gray')
+	axarr[1, 0].imshow(yellowimg)
+	axarr[1, 1].imshow(yellow_masked_edges, cmap='gray')
+	plt.show()
+
+def region_of_interest(image, vertices):
+    mask = np.zeros_like(image)   
+    
+    if len(image.shape) > 2:
+        channel_count = image.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+        
+    cv2.fillPoly(mask, [vertices], ignore_mask_color)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+def draw_region(img, vertices):
+	cp = np.copy(img)
+	numl = len(vertices)
+	for i, j in zip(range(numl), range(1, numl + 1)):
+		cv2.line(cp, tuple(vertices[i % numl]), tuple(vertices[j % numl]), (255,0,0), 5)
+	return cp
+
+def show_regions_of_interest():
+	height = img.shape[0]
+	width = img.shape[1]
+	vertices = np.array([(0.05,height),(0.45*width, 0.55*height), (0.55*width, 0.55*height),
+		(0.95*width, height)], dtype=np.int32)
+	f, axarr = plt.subplots(2, 2, figsize=(20, 12))
+	axarr[0, 0].imshow(draw_region(img, vertices))
+	axarr[0, 1].imshow(region_of_interest(img, vertices))
+	axarr[1, 0].imshow(draw_region(yellowimg, vertices))
+	axarr[1, 1].imshow(region_of_interest(yellowimg, vertices))
+	plt.show()
