70c2aaed077445346cb1ddfa29bbaadf3082c238
Kristen Holtz <holtz.kristen@gmail.com>
2018-03-16 03:54:05 -0700

    Hough transform code in place but not interactive; video added

--- secret_sauce.py
+++ secret_sauce.py
@@ -15,6 +15,19 @@ class Params:
 
 params = Params()
 
+# Load Images
+filename = 'solidWhiteRight.jpg'
+img = mpimg.imread(filename)
+yellowfile = 'solidYellowCurve.jpg'
+yellowimg = mpimg.imread(yellowfile)
+
+# Define region of interest (ROI) in images
+height = img.shape[0]
+width = img.shape[1]
+vertices = np.array([(0.05,height),(0.45*width, 0.55*height), (0.55*width, 0.55*height),
+	(0.95*width, height)], dtype=np.int32)
+
+# Create widgets
 def get_color_widget(color_name):
 	return widgets.IntRangeSlider(
 		value=[0, 255], 
@@ -58,12 +71,7 @@ high_widget = widgets.IntSlider(
 	readout_format='d'
 )
 
-filename = 'solidWhiteRight.jpg'
-img = mpimg.imread(filename)
-
-yellowfile = 'solidYellowCurve.jpg'
-yellowimg = mpimg.imread(yellowfile)
-
+# Color thresholding
 def apply_color_thresholds(image, red, blue, green):
 	out_of_range = (image[:, :, 0] < red[0]) | (image[:, :, 0] > red[1]) \
 	| (image[:, :, 1] < blue[0]) | (image[:, :, 1] > blue[1]) \
@@ -84,30 +92,7 @@ def show_color_threshold_results(red=(0, 255), blue=(0, 255), green=(0, 255)):
 	axarr[1, 1].imshow(apply_color_thresholds(yellowimg, red, blue, green))
 	plt.show()
 
-def show_rgb():
-	f, axarr = plt.subplots(1, 3, figsize=(20,10))
-	red_img = np.copy(img)
-	red_img[:,:,1] = 0
-	red_img[:,:,2] = 0
-	axarr[0].imshow(red_img)
-	
-	blue_img = np.copy(img)
-	blue_img[:,:,0] = 0
-	blue_img[:,:,2] = 0
-	axarr[1].imshow(blue_img)
-
-	green_img = np.copy(img)
-	green_img[:,:,0] = 0
-	green_img[:,:,1] = 0
-	axarr[2].imshow(green_img)
-	plt.show()
-	
-	f, axarr = plt.subplots(1, 3, figsize=(20,10))
-	axarr[0].imshow(red_img[:,:,0], cmap='gray')
-	axarr[1].imshow(blue_img[:,:,1], cmap='gray')
-	axarr[2].imshow(green_img[:,:,2], cmap='gray')
-	plt.show()
-
+# Edge Detection
 def apply_edges(image, low_threshold, high_threshold):
 	gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 	return cv2.Canny(gray, low_threshold, high_threshold)
@@ -122,29 +107,28 @@ def show_edge_detection_results(low_threshold=0, high_threshold=0):
 	axarr[1, 1].imshow(apply_edges(yellowimg, low_threshold, high_threshold), cmap='gray')
 	plt.show()
 
-def apply_masks():
+# Cumulative color and edge detection
+def apply_masks(image):
 	lower_color = np.array([params.red[0], params.blue[0], params.green[0]])
 	higher_color = np.array([params.red[1], params.blue[1], params.green[1]])
-	colormask_white = cv2.inRange(img, lower_color, higher_color)
-	colormask_white = cv2.dilate(colormask_white, (10, 10))
-	white_edges = cv2.Canny(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), 
-		params.low_threshold, params.high_threshold)
-	white_masked_edges = cv2.bitwise_and(white_edges, colormask_white)
 
-	colormask_yellow = cv2.inRange(yellowimg, lower_color, higher_color)
-	colormask_yellow = cv2.dilate(colormask_yellow, (10, 10))
-	yellow_edges = cv2.Canny(cv2.cvtColor(yellowimg, cv2.COLOR_RGB2GRAY), 
+	colormask = cv2.inRange(image, lower_color, higher_color)
+	colormask = cv2.dilate(colormask, (10, 10))
+	edges = cv2.Canny(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY),
 		params.low_threshold, params.high_threshold)
-	yellow_masked_edges = cv2.bitwise_and(yellow_edges, colormask_yellow)
+	return cv2.bitwise_and(edges, colormask)
 
+def show_masking_results():
 	f, axarr = plt.subplots(2,2, figsize=(20, 12))
 	axarr[0, 0].imshow(img)
-	axarr[0, 1].imshow(white_masked_edges, cmap='gray')
+	axarr[0, 1].imshow(apply_masks(img), cmap='gray')
 	axarr[1, 0].imshow(yellowimg)
-	axarr[1, 1].imshow(yellow_masked_edges, cmap='gray')
+	axarr[1, 1].imshow(apply_masks(yellowimg), cmap='gray')
 	plt.show()
 
-def region_of_interest(image, vertices):
+# ROI
+# TODO: give them sliders for the ROI
+def region_of_interest(image):
     mask = np.zeros_like(image)   
     
     if len(image.shape) > 2:
@@ -157,7 +141,7 @@ def region_of_interest(image, vertices):
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
-def draw_region(img, vertices):
+def draw_region(img):
 	cp = np.copy(img)
 	numl = len(vertices)
 	for i, j in zip(range(numl), range(1, numl + 1)):
@@ -165,13 +149,36 @@ def draw_region(img, vertices):
 	return cp
 
 def show_regions_of_interest():
-	height = img.shape[0]
-	width = img.shape[1]
-	vertices = np.array([(0.05,height),(0.45*width, 0.55*height), (0.55*width, 0.55*height),
-		(0.95*width, height)], dtype=np.int32)
 	f, axarr = plt.subplots(2, 2, figsize=(20, 12))
-	axarr[0, 0].imshow(draw_region(img, vertices))
-	axarr[0, 1].imshow(region_of_interest(img, vertices))
-	axarr[1, 0].imshow(draw_region(yellowimg, vertices))
-	axarr[1, 1].imshow(region_of_interest(yellowimg, vertices))
+	axarr[0, 0].imshow(draw_region(img))
+	axarr[0, 1].imshow(region_of_interest(img))
+	axarr[1, 0].imshow(draw_region(yellowimg))
+	axarr[1, 1].imshow(region_of_interest(yellowimg))
 	plt.show()
+
+# Hough Lines
+def get_hough_line_image(image):
+	# Define the Hough transform parameters
+    rho = 1 # distance resolution in pixels of the Hough grid
+    theta = 6*np.pi/180 # angular resolution in radians of the Hough grid
+    threshold = 20     # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 12 #minimum number of pixels making up a line
+    max_line_gap = 20  # maximum gap in pixels between connectable line segments
+    line_image = np.copy(image)*0 # creating a blank to draw lines on
+
+    masked_edges = region_of_interest(apply_masks(image))
+    # Run Hough on edge detected image
+    lines = cv2.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), 
+    	min_line_length, max_line_gap)
+
+    for l in lines:
+    	cv2.line(line_image, (l[0, 0], l[0, 1]), (l[0, 2], l[0, 3]), (255, 0, 0), 10)
+    masked_edges_color = np.dstack((masked_edges, masked_edges, masked_edges))
+    return cv2.addWeighted(masked_edges_color, 0.7, line_image, 1, 0)
+
+def show_hough_lines():
+	f, axarr = plt.subplots(1, 2, figsize=(20, 12))
+	axarr[0].imshow(get_hough_line_image(img))
+	axarr[1].imshow(get_hough_line_image(yellowimg))
+	plt.show()
+
